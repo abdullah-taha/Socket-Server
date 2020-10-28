13c66feeaf4638af3cd0d4586e6bcfc1ef49dcb2
Abdullah Taha <abdullah-taha13@hotmail.com>
2020-10-28 19:41:08 +0300

    commıt

--- client.py
+++ client.py
@@ -9,13 +9,24 @@ port = 9998
 s.connect((host,port))
 
 while True:
-    data = s.recv(1024)
-    if(data[:2].decode("utf-8") == "cd"):
-        os.chdir(data[3:].decode("utf-8"))
-    if(len(data)>0):
-        cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout = subprocess.PIPE, stdin = subprocess.PIPE, stderr=subprocess.PIPE)
-        output_byte = cmd.stdout.read() + cmd.stderr.read()
-        output_str = str(output_byte,"utf-8")
-        current_wd = os.getcwd() + "> "
-        s.send(str.encode(output_str + current_wd))
-        print(output_str)
+    cmd = input()
+    if(cmd=="q"):
+        s.close()
+        break
+
+    if(len(cmd) > 0):
+        s.send(str.encode(cmd))
+        client_response = str(s.recv(1024),"utf-8")
+        print(client_response, end="")
+print("goodbye")
+
+    # data = s.recv(1024)
+    # if(data[:2].decode("utf-8") == "cd"):
+    #     os.chdir(data[3:].decode("utf-8"))
+    # if(len(data)>0):
+    #     cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout = subprocess.PIPE, stdin = subprocess.PIPE, stderr=subprocess.PIPE)
+    #     output_byte = cmd.stdout.read() + cmd.stderr.read()
+    #     output_str = str(output_byte,"utf-8")
+    #     current_wd = os.getcwd() + "> "
+    #     s.send(str.encode(output_str + current_wd))
+    #     print(output_str)
--- server.py
+++ server.py
@@ -2,8 +2,9 @@ import socket
 import sys
 import threading 
 from queue import Queue
-num_of_threads = 2
-job_num = [1,2]
+import os
+import subprocess
+import threading
 
 host = ""
 port = 9998
@@ -32,11 +33,36 @@ def bind_socket():
         bind_socket()
 
 
-def socket_accept():
-    conn,addres = s.accept()
-    print("connection estableshed. IP: "+addres[0] + ", port: "+str(addres[1]))
-    send_command(conn)
-    conn.close()
+# def socket_accept():
+#     conn,addres = s.accept()
+#     print("connection estableshed. IP: "+addres[0] + ", port: "+str(addres[1]))
+#     receive_command(conn)
+#     #send_command(conn)
+#     conn.close()
+
+def receive_command(conn):
+    print(threading.current_thread().name)
+    cwd = os.getcwd()
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            print("clinet disconnected")
+            conn.close()
+            break
+        if(data[:2].decode("utf-8") == "cd"):
+            #os.chdir(data[3:].decode("utf-8"))
+            cwd = +data[3:].decode("utf-8")
+            print(cwd)
+        if(len(data)>0):
+            cmd = subprocess.Popen(data[:].decode("utf-8"),shell=True,  stdout = subprocess.PIPE, stdin = subprocess.PIPE, stderr=subprocess.PIPE,cwd=cwd)
+            if(data[:2].decode("utf-8") == "cd"):
+                output_str=""
+            else:
+                output_byte = cmd.stdout.read() + cmd.stderr.read()
+                output_str = str(output_byte,"utf-8")
+            current_wd = cwd + "> "
+            conn.send(str.encode(output_str + current_wd))
+            #print(output_str)
 
 def send_command(conn):
     while True:
@@ -52,9 +78,27 @@ def send_command(conn):
             client_response = str(conn.recv(1024),"utf-8")
             print(client_response, end="")
 
+def socket_accept(client, connection):
+    ip = connection[0]
+    port = connection[1]
+    print(f"THe new connection was made from IP: {ip}, and port: {port}!")
+    receive_command(client)
+    print(f"The client from ip: {ip}, and port: {port}, has gracefully diconnected!")
+    client.close()
+
+
 def main():
     create_socket()
     bind_socket()
-    socket_accept()
+    while True:
+        try: 
+            client, ip = s.accept()
+            threading._start_new_thread(socket_accept,(client, ip))
+        except KeyboardInterrupt:
+            print(f"Gracefully shutting down the server!")
+        except Exception as e:
+            print(f"Well I did not anticipate this: {e}")
+    #socket_accept()
+
 
 main()
